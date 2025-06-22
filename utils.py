11f16ec69021cf4cfe49ae2e41c2905947e7e804
“shamim-github” <shamim.ahmed2017@icloud.com>
2025-06-22 20:46:15 +0530

    Resolved .gitignore merge conflict

--- utils.py
+++ utils.py
@@ -4,5 +4,5 @@ import numpy as np
 def preprocess_image(image_file, target_size=(224, 224)):
     img = Image.open(image_file).convert("RGB")
     img_resized = img.resize(target_size)
-    img_array = np.array(img_resized) / 255.0
-    return np.expand_dims(img_array, axis=0)
+    img_array = np.array(img_resized) / 255.0  # Normalize pixel values to [0, 1]
+    return np.expand_dims(img_array, axis=0)    # Add batch dimension
